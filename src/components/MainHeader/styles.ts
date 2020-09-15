f5fc886738842e3545131cb30590717376e2460e
Tamires <tamires.manhaes@cubos.io>
2020-09-15 13:52:29 -0300

    adicionando estilização inicial do header

--- src/components/MainHeader/styles.ts
+++ src/components/MainHeader/styles.ts
@@ -4,4 +4,22 @@ export const Container = styled.div`
   grid-area: MAINHEADER;
   background: ${props => props.theme.colors.secondary};
   color: ${props => props.theme.colors.white};
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 0 15px;
+  border-bottom: 1px solid ${props => props.theme.colors.white};
 `;
+
+export const Profile = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Welcome = styled.h3`
+  display: flex;
+`;
+
+export const Username = styled.span``;
