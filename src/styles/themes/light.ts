bafcbfc4b68d75b720056512f70cdf1c5177a094
Tamires <tamires.manhaes@cubos.io>
2020-09-15 11:13:34 -0300

    adicionando arquivos dos temas

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,19 @@
+export default {
+  title: 'dark',
+
+  color: {
+    primary: '#DCDCDC',
+    secondary: '#FFFFFF',
+    tertiary: '#F5F5F5',
+
+    white: '#FFF',
+    black: '#000',
+    grey: '#BFBFBF',
+
+    success: '#03BB85',
+    info: '#3B5998',
+    warning: '#FF6961'
+  },
+
+
+};
