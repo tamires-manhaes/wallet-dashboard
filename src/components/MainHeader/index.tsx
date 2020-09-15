9bf2ac11288f2a3075018eb2a7b8f9a2634e24c9
Tamires <tamires.manhaes@cubos.io>
2020-09-15 13:52:10 -0300

    adiciando lógica de renderização random do emoji

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
-import { Container } from './styles';
+import { Container, Profile, Welcome, Username } from './styles';
+
+import emojis from '../../utils/emojis';
 
 const MainHeader: React.FC = () => {
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length );
+    return emojis[indice];
+  }, []);
+
   return(
     <Container>
       <h1>MainHeader</h1>
+
+      <Profile>
+        <Welcome>Olar!{' '} {emoji}</Welcome>
+        <Username>Tamires Lynda</Username>
+      </Profile>
     </Container>
   )
 }
