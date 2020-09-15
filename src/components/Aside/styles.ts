8ed7816df6ce06fbbf9de2a5b53dc84ba2ff23cb
Tamires <tamires.manhaes@cubos.io>
2020-09-15 11:47:52 -0300

    adicinando cores do tema

--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -2,5 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   grid-area: ASIDE;
-  background: var(--light-purple);
+  background: ${props => props.theme.colors.secondary};
+  color: ${props => props.theme.colors.white}
 `;
