c60c5e1855dba95b9b9c532a303e6cb115d7286c
Tamires <tamires.manhaes@cubos.io>
2020-09-16 14:36:28 -0300

    adicionando estrutura inicial

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
 import ContentHeader from '../../components/ContentHeader';
+import SelectInput from '../../components/SelectInput';
 import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
+  const options = [{ value: 'Tam', label: 'Tamires' }, { value: 'Br', label: 'Brooke' }, { value: 'Lol', label: 'Lola' }];
+  
   return (
     <Container>
-      <ContentHeader />
-      <h1>COntent</h1>
+      <ContentHeader title="Dashboard" lineColor="#fff">
+        <SelectInput options={options}/>
+      </ContentHeader>
     </Container>
   )
 }
