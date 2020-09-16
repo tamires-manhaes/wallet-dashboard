2fb25e19e7c71be032b1257a59c7fe41684c5019
Tamires <tamires.manhaes@cubos.io>
2020-09-16 13:38:44 -0300

    adicionando component do corpo do dashboard

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -4,4 +4,6 @@ export const Container = styled.div`
   grid-area: CONTENT;
   background: ${props => props.theme.colors.primary};
   color: ${props => props.theme.colors.white};
+
+  padding: 25px;
 `;
