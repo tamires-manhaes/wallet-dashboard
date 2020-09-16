5e3b9b193e65c671341b4db56bd9357d2482d884
Tamires <tamires.manhaes@cubos.io>
2020-09-16 14:37:44 -0300

    finalizando estrutura e estilo do componente

---
+++ src/components/SelectInput/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  
+  > select {
+    padding: 5px 10px;
+    border-radius: 5px;
+    border: 1px solid ${props => props.theme.colors.grey};
+    background: ${props => props.theme.colors.white};
+
+    margin-left: 5px;
+  }
+`;
