b50585ee65065376179ccf6b0e7ec6a55934e44c
Tamires <tamires.manhaes@cubos.io>
2020-09-16 16:23:13 -0300

    adicionando componente de card

---
+++ src/components/HistoryFinanceCard/styles.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+interface iContainerProps {
+  color: string,
+}
+
+interface iTagPros {
+  color: string,
+}
+
+export const Container = styled.li<iContainerProps>`
+  position: relative;
+  background: ${props => props.color};
+  list-style: none;
+
+  padding: 10px 15px;
+  margin: 10px 0;
+  border-radius: 5px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  cursor: pointer;
+
+  transition: all 350ms ease;
+
+  &:hover {
+    opacity: .8;
+    transform: translateX(10px);
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding-left: 15px;
+  }
+`;
+
+export const Tag = styled.div<iTagPros>`
+  background: ${props => props.color};
+  position: absolute;
+  top: 50%;
+  left: 0;
+  transform: translate(0%, -50%);
+  width: 8px;
+  height: 60%;
+`;
