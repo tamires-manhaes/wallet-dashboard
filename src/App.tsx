77be76b987af27cf57d0d71322994330bd3f8c79
Tamires <tamires.manhaes@cubos.io>
2020-09-16 13:41:40 -0300

    comemtando import do tema, adicionando container no layout e do dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 
 import Layout from './components/Layout';
+import Dashboard from './pages/Dashboard';
 import dark from './styles/themes/dark';
-import light from './styles/themes/light';
+// import light from './styles/themes/light';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={dark}>
       <GlobalStyles />
-      <Layout />
+      <Layout>
+        <Dashboard />
+      </Layout>
     </ThemeProvider>
   )
 }
