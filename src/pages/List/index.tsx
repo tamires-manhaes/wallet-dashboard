71f7215e067f858e191582eb772a4ee8dcbbd178
Tamires <tamires.manhaes@cubos.io>
2020-09-16 16:23:34 -0300

    criação e eslitilização inicial da lista finalizada

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Content } from './styles';
 
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
+import HistoryCard from '../../components/HistoryFinanceCard';
 
 const List: React.FC = () => {
   const options = [{ value: 'Tam', label: 'Tamires' }, { value: 'Br', label: 'Brooke' }, { value: 'Lol', label: 'Lola' }];
@@ -13,6 +14,18 @@ const List: React.FC = () => {
       <ContentHeader title="Entradas" lineColor="#E44FCE">
         <SelectInput options={options}/>
       </ContentHeader>
+
+      <Content>
+        <HistoryCard  cardColor="#313862" tagColor="#F7931B" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+        <HistoryCard  cardColor="#313862" tagColor="#E44FCE" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+        <HistoryCard  cardColor="#313862" tagColor="#E44C4E" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+        <HistoryCard  cardColor="#313862" tagColor="#F7931B" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+        <HistoryCard  cardColor="#313862" tagColor="#E44C4E" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+        <HistoryCard  cardColor="#313862" tagColor="#E44FCE" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+        <HistoryCard  cardColor="#313862" tagColor="#F7931B" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+        <HistoryCard  cardColor="#313862" tagColor="#E44FCE" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+        <HistoryCard  cardColor="#313862" tagColor="#E44C4E" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+      </Content>
     </Container>
   )
 }
