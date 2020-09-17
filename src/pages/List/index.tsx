4bdf58d702e1859b7ce44c7f1a89ef4920b1bd9c
Tamires <tamires.manhaes@cubos.io>
2020-09-17 10:58:52 -0300

    adiconando filtros

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,30 +1,57 @@
 import React from 'react';
 
-import { Container, Content } from './styles';
+import { Container, Content, Filters } from './styles';
 
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 import HistoryCard from '../../components/HistoryFinanceCard';
 
 const List: React.FC = () => {
-  const options = [{ value: 'Tam', label: 'Tamires' }, { value: 'Br', label: 'Brooke' }, { value: 'Lol', label: 'Lola' }];
+  const months = [
+    { value: 0, label: 'Mês' }, 
+    { value: 3, label: 'Março' }, 
+    { value: 4, label: 'Abril' }, 
+    { value: 5, label: 'Maio' },
+    { value: 6, label: 'Junho' },
+    { value: 7, label: 'Julho' },
+    { value: 8, label: 'Agosto' },
+    { value: 9, label: 'Setembro' },
+    { value: 10, label: 'Outubro' },
+    { value: 11, label: 'Novembro' },
+    { value: 12, label: 'Dezembro' },
+  ];
+
+  const years = [
+    { value: 0, label: 'Ano' }, 
+    { value: 2016, label: '2016' },
+    { value: 2017, label: '2017' },
+    { value: 2018, label: '2018' },
+    { value: 2019, label: '2019' },
+    { value: 2020, label: '2020' },
+  ]
 
   return (
     <Container>
       <ContentHeader title="Entradas" lineColor="#E44FCE">
-        <SelectInput options={options}/>
+        <SelectInput options={months}/>
+        <SelectInput options={years}/>
       </ContentHeader>
 
+      <Filters>
+        <button type="button" className="tag-filter tag-filter-eventuals">Eventuais</button>
+        <button type="button" className="tag-filter tag-filter-recurrent">Recorrentes</button>
+      </Filters>
+
       <Content>
-        <HistoryCard  cardColor="#313862" tagColor="#F7931B" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
-        <HistoryCard  cardColor="#313862" tagColor="#E44FCE" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
-        <HistoryCard  cardColor="#313862" tagColor="#E44C4E" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
-        <HistoryCard  cardColor="#313862" tagColor="#F7931B" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
-        <HistoryCard  cardColor="#313862" tagColor="#E44C4E" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
-        <HistoryCard  cardColor="#313862" tagColor="#E44FCE" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
-        <HistoryCard  cardColor="#313862" tagColor="#F7931B" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
-        <HistoryCard  cardColor="#313862" tagColor="#E44FCE" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
-        <HistoryCard  cardColor="#313862" tagColor="#E44C4E" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+        <HistoryCard tagColor="#F7931B" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+        <HistoryCard tagColor="#E44FCE" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+        <HistoryCard tagColor="#E44C4E" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+        <HistoryCard tagColor="#F7931B" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+        <HistoryCard tagColor="#E44C4E" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+        <HistoryCard tagColor="#E44FCE" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+        <HistoryCard tagColor="#F7931B" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+        <HistoryCard tagColor="#E44FCE" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
+        <HistoryCard tagColor="#E44C4E" title="Running shoes" subtitle="15/09/2020" amount="$500"/>
       </Content>
     </Container>
   )
--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -7,3 +7,42 @@ export const Container = styled.div`
 export const Content = styled.ul`
 
 `;
+
+export const Filters = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+
+  .tag-filter{
+    position: relative;
+    font-size: 18px;
+    font-weight: 500;
+    background: none;
+    border: none;
+    color: ${props => props.theme.colors.white};
+
+    margin: 0 10px;
+    transition: opacity 350ms ease;
+
+    &:hover {
+      opacity: .8;
+    }
+
+    &-recurrent::after {
+      content: '';
+      display: block;
+      width: 55px;
+      margin: 0 auto;
+      border-bottom: 10px solid ${props => props.theme.colors.warning};
+    }
+
+    &-eventuals::after{
+      content: '';
+      display: block;
+      width: 55px;
+      margin: 0 auto;
+      border-bottom: 10px solid ${props => props.theme.colors.success};
+    }
+  }
+`;
