1c7214f812075e0b02ea0b6874ccfdfa125988ac
Tamires <tamires.manhaes@cubos.io>
2020-09-17 11:00:01 -0300

    adicinando largura mínima do input

--- src/components/SelectInput/styles.ts
+++ src/components/SelectInput/styles.ts
@@ -4,6 +4,7 @@ export const Container = styled.div`
   display: flex;
   
   > select {
+    min-width: 110px;
     padding: 5px 10px;
     border-radius: 5px;
     border: 1px solid ${props => props.theme.colors.grey};
