9b6915017605c9fc3ea8cb22211e8a5ee45e961d
Tamires <tamiresmanhaes4@gmail.com>
2020-09-18 14:53:45 -0300

    refatorando função de formatação de data

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,10 @@
 
-export const parseStringAsArray = (arrayAsString: string) => {
-  let dArr = arrayAsString.split("-"); 
-  return dArr[2]+ "/" +dArr[1]+ "/" +dArr[0]
+export const parseStringAsArray = (date: string) => {
+  const dateFormatted = new Date(date);
+
+  const year = dateFormatted.getFullYear();
+  const month = dateFormatted.getMonth() + 1 > 9 ? dateFormatted.getMonth() + 1 : `0${dateFormatted.getMonth() + 1 }`;
+  const day = dateFormatted.getDate() > 9 ? dateFormatted.getDate() : `0${dateFormatted.getDate()}`;
+  
+  return `${day}/${month}/${year}`;
 };
